173da21ff23f55a5312c47000800b9d842f166d4
Christian <cosechasistema@gmail.com>
2022-04-04 20:05:06 -0300

    Cambio para ver el status

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var axios = require("axios");
 var mysql = require("mysql");
 
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 // Para Pasarlo a docker https://nodejs.org/de/docs/guides/nodejs-docker-webapp/
 require("dotenv").config();
 
